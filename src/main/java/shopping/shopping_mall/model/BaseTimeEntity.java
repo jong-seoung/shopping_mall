5c296cf898d2f0c2caa01303557b4be2e9630d0e
jong-seoung <jjong015189@gmail.com>
2025-04-07 18:45:43 +0900

    Auditing & 오타 수정
    jpa Auditing 기능 추가 및 item 모델 오타 수정

--- src/main/java/shopping/shopping_mall/model/BaseTimeEntity.java
+++ src/main/java/shopping/shopping_mall/model/BaseTimeEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -20,6 +20,6 @@ public abstract class BaseTimeEntity {
     @Column(updatable = false)
     private LocalDateTime regTime;
 
-    @LastModifiedBy
+    @LastModifiedDate
     private LocalDateTime updateTime;
 }
